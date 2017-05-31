27e1d8e176952101adb89db7e0880dc7abe3e610
Preethirjpm <preethirjpm@gmail.com>
2017-06-01 01:22:53 +0530

    JAVA ASSIGNMENT-7.1
    Write a program to convert an int variable to String using an inbuilt function of String class.

---
+++ StringDemo.java
@@ -0,0 +1,15 @@
+public class StringDemo {
+
+public static void main(String[] args) {
+
+int i_am_int = 756;
+
+//Integer to String conversion
+
+   String str1 = String.valueOf(i_am_int);
+
+    System.out.println("This is the conversion from Integer to String " + str1);
+
+   }
+
+}
